36c2da47771b1c618824738b338f17598a805fd9
Aayan356 <110051@sl.balbharati.org>
2026-08-29 10:45:07 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://yapudwtprynmdstrxkmt.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY ="sb_publishable_ijwk62pv-GiYhTziY8CBxQ_knm_dXIP"
 
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
